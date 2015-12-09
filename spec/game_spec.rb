502150ae47a4b7beafc9897fbfa1dbda1e893d3d
Kirk Smith <kirks@hotmail.co.uk>
2015-12-09 17:39:33 +0000

    step#26

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,7 +1,7 @@
 require 'game'
 
 describe Game do
-  let(:game) {described_class.new}
+  let(:game) {described_class.new(player_1,player_2)}
   let(:player_1) {double :player}
   let(:player_2) {double :player}
 
@@ -10,6 +10,14 @@ describe '#attack' do
     expect(player_2).to receive(:receive_damage)
     game.attack(player_2)
   end
+end
+
+it 'retrieve names for player_1' do
+  expect(game.player_1).to eq player_1
+end
+
+it 'retrieve names for player_2' do
+  expect(game.player_2).to eq player_2
 end
 
 end
