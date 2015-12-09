adb0eaa5e15eb246fd483551878b4a156739b9f6
Jelgar <jonathanelgardiner@gmail.com>
2015-12-09 17:47:53 +0000

    game class delegator

--- lib/game.rb
+++ lib/game.rb
@@ -1,4 +1,13 @@
+require 'forwardable'
+
 class Game
+  extend Forwardable
+
+  def_delegator :player_1, :name, :player_1_name
+  def_delegator :player_1, :hp, :player_1_hp
+  def_delegator :player_2, :name, :player_2_name
+  def_delegator :player_2, :hp, :player_2_hp
+
   attr_reader :player_1, :player_2
 
   def initialize(player_1, player_2)
